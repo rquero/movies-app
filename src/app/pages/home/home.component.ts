46dcfae4c5fff0940b298f38f476dfc7ee6bf7b4
rquero <ricardoequero@gmail.com>
2025-01-10 16:08:40 -0400

    feat: crear listado de peliculas

---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,19 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss'
+})
+export class HomeComponent {
+	movies: String[] = [
+		'Star Wars',
+		'Harry Potter',
+		'Avengers',
+		'Batman',
+	];
+
+}
